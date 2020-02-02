6685976a5c7206c3d1f6e0b501f921de17f0f7eb
Fernatha <nfernandezbrown@gmail.com>
2020-02-02 15:36:07 -0500

    alien ai update
    alien no longer walks off edge into the endless void

--- Assets/Jetroid/Scripts/LookForward.cs
+++ Assets/Jetroid/Scripts/LookForward.cs
@@ -6,6 +6,8 @@ public class LookForward : MonoBehaviour
 {
 
     public Transform sightStart, sightEnd;
+    public string layer = "Solid";
+    public bool needsCollision = true;
 
     private bool collision;
     // Start is called before the first frame update
@@ -17,11 +19,11 @@ public class LookForward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        collision = Physics2D.Linecast(sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer("Solid"));
+        collision = Physics2D.Linecast(sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer(layer));
 
         Debug.DrawLine(sightStart.position, sightEnd.position, Color.green);
 
-        if (collision)
+        if (collision == needsCollision)
         {
             transform.localScale = new Vector3(transform.localScale.x == 1 ? -1 : 1, 1, 1);
         }
