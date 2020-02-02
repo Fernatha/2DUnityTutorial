baaddf66a95734c1e9b8538f4ffeebb0891d777b
Fernatha <nfernandezbrown@gmail.com>
2020-02-02 13:31:11 -0500

    Debris update
    Debris prefab created, lil explodey boi has debris on death

---
+++ Assets/Jetroid/Scripts/Debris.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debris : MonoBehaviour
+{
+    private SpriteRenderer rendered2D;
+    private Color start;
+    private Color end;
+    private float t = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rendered2D = GetComponent<SpriteRenderer>();
+        start = rendered2D.color;
+        end = new Color(start.r, start.g, start.b, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        rendered2D.material.color = Color.Lerp(start, end, t / 2);
+
+        if(rendered2D.material.color.a <= 0f)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
--- Assets/Jetroid/Scripts/Explode.cs
+++ Assets/Jetroid/Scripts/Explode.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Explode : MonoBehaviour
 {
+    public Debris debris;
+    public int totalDebris = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +29,17 @@ public class Explode : MonoBehaviour
 
     void OnExplode()
     {
+        var t = transform;
+
+        for(int i = 0; i < totalDebris; i++)
+        {
+            t.TransformPoint(0, -100, 0);
+            var clone = Instantiate(debris, t.position, Quaternion.identity) as Debris;
+            var body2D = clone.GetComponent<Rigidbody2D>();
+            body2D.AddForce(Vector3.right * Random.Range(-1000, 1000));
+            body2D.AddForce(Vector3.up * Random.Range(500, 2000));
+        }
+
         Destroy(gameObject);
     }
 }
